7ce33d654d8f20720ce56751580bdd63d63ec327
Anjali G <anjali.p@perfios.com>
2022-04-25 18:56:25 +0530

    added changes for scraping in puppeteer, generic selectors

--- Puppeteer/Axis/axisscrape.js
+++ Puppeteer/Axis/axisscrape.js
@@ -10,28 +10,40 @@ const noop = () => {};
 async function parse() {
     var personal_details = {}
     var $ = cheerio.load(fs.readFileSync('PersonalDetails.html'));
-    var date = $('body > app-root > mat-sidenav-container > mat-sidenav-content > div > app-homepage > div > div > mat-sidenav-container > mat-sidenav-content > div.innerContheight.ng-tns-c6-0 > section > app-s2000 > div > div > div.tabs.accounttabs.m-b-none > div > app-account-details > div > div > div:nth-child(1) > div.vertical-divider-accountDetails > app-hdr-list-link > div:nth-child(6) > div > div').text()
-    personal_details["pidata"]={
-        Account_Holder_Name:$('body > app-root > mat-sidenav-container > mat-sidenav-content > div > app-homepage > div > div > mat-sidenav-container > mat-sidenav-content > div.innerContheight.ng-tns-c6-0 > section > app-s2000 > div > div > div.tabs.accounttabs.m-b-none > div > app-account-details > div > div > div:nth-child(1) > div.vertical-divider-accountDetails > app-hdr-list-link > div:nth-child(7) > div > div').text(),
-        Account_Type:$('body > app-root > mat-sidenav-container > mat-sidenav-content > div > app-homepage > div > div > mat-sidenav-container > mat-sidenav-content > div.innerContheight.ng-tns-c6-0 > section > app-s2000 > div > div > div.tabs.accounttabs.m-b-none > div > app-account-details > div > div > div:nth-child(1) > div.vertical-divider-accountDetails > app-hdr-list-link > div:nth-child(3) > div > div').text(),
-        Currency:$('body > app-root > mat-sidenav-container > mat-sidenav-content > div > app-homepage > div > div > mat-sidenav-container > mat-sidenav-content > div.innerContheight.ng-tns-c6-0 > section > app-s2000 > div > div > div.tabs.accounttabs.m-b-none > div > app-account-details > div > div > div:nth-child(1) > div.vertical-divider-accountDetails > app-hdr-list-link > div:nth-child(4) > div > div').text(),
-        Date_Of_Opening:date.slice(7)+"-"+date.slice(3,6)+"-"+date.slice(0,2),
-        IFSC_Code:$('body > app-root > mat-sidenav-container > mat-sidenav-content > div > app-homepage > div > div > mat-sidenav-container > mat-sidenav-content > div.innerContheight.ng-tns-c6-0 > section > app-s2000 > div > div > div.tabs.accounttabs.m-b-none > div > app-account-details > div > div > div:nth-child(1) > div.vertical-divider-accountDetails > app-hdr-list-link > div:nth-child(9) > div > div').text(),
-        Status:$('body > app-root > mat-sidenav-container > mat-sidenav-content > div > app-homepage > div > div > mat-sidenav-container > mat-sidenav-content > div.innerContheight.ng-tns-c6-0 > section > app-s2000 > div > div > div.tabs.accounttabs.m-b-none > div > app-account-details > div > div > div:nth-child(1) > div.vertical-divider-accountDetails > app-hdr-list-link > div:nth-child(14) > div > div').text(),
+    var account_holder_name = $('label:contains("Account holder")').next().text();
+    var account_type = $('label:contains("Account type")').next().text();
+    var currency = $('label:contains("Currency")').next().text();
+    // fetch the other details mentioned, follow the same for Account summary
+    personal_details = {
+        "accountHolderName":account_holder_name,
+        "accountTYpe":account_type,
+        "currency":currency
     }
-    console.log(personal_details)
-    fs.appendFile("output.txt",String(personal_details),noop)
+    // var date = $('body > app-root > mat-sidenav-container > mat-sidenav-content > div > app-homepage > div > div > mat-sidenav-container > mat-sidenav-content > div.innerContheight.ng-tns-c6-0 > section > app-s2000 > div > div > div.tabs.accounttabs.m-b-none > div > app-account-details > div > div > div:nth-child(1) > div.vertical-divider-accountDetails > app-hdr-list-link > div:nth-child(6) > div > div').text()
+    // personal_details["pidata"]={
+    //     Account_Holder_Name:$('body > app-root > mat-sidenav-container > mat-sidenav-content > div > app-homepage > div > div > mat-sidenav-container > mat-sidenav-content > div.innerContheight.ng-tns-c6-0 > section > app-s2000 > div > div > div.tabs.accounttabs.m-b-none > div > app-account-details > div > div > div:nth-child(1) > div.vertical-divider-accountDetails > app-hdr-list-link > div:nth-child(7) > div > div').text(),
+    //     Account_Type:$('body > app-root > mat-sidenav-container > mat-sidenav-content > div > app-homepage > div > div > mat-sidenav-container > mat-sidenav-content > div.innerContheight.ng-tns-c6-0 > section > app-s2000 > div > div > div.tabs.accounttabs.m-b-none > div > app-account-details > div > div > div:nth-child(1) > div.vertical-divider-accountDetails > app-hdr-list-link > div:nth-child(3) > div > div').text(),
+    //     Currency:$('body > app-root > mat-sidenav-container > mat-sidenav-content > div > app-homepage > div > div > mat-sidenav-container > mat-sidenav-content > div.innerContheight.ng-tns-c6-0 > section > app-s2000 > div > div > div.tabs.accounttabs.m-b-none > div > app-account-details > div > div > div:nth-child(1) > div.vertical-divider-accountDetails > app-hdr-list-link > div:nth-child(4) > div > div').text(),
+    //     Date_Of_Opening:date.slice(7)+"-"+date.slice(3,6)+"-"+date.slice(0,2),
+    //     IFSC_Code:$('body > app-root > mat-sidenav-container > mat-sidenav-content > div > app-homepage > div > div > mat-sidenav-container > mat-sidenav-content > div.innerContheight.ng-tns-c6-0 > section > app-s2000 > div > div > div.tabs.accounttabs.m-b-none > div > app-account-details > div > div > div:nth-child(1) > div.vertical-divider-accountDetails > app-hdr-list-link > div:nth-child(9) > div > div').text(),
+    //     Status:$('body > app-root > mat-sidenav-container > mat-sidenav-content > div > app-homepage > div > div > mat-sidenav-container > mat-sidenav-content > div.innerContheight.ng-tns-c6-0 > section > app-s2000 > div > div > div.tabs.accounttabs.m-b-none > div > app-account-details > div > div > div:nth-child(1) > div.vertical-divider-accountDetails > app-hdr-list-link > div:nth-child(14) > div > div').text(),
+    // }
+    //console.log(personal_details)
+    fs.appendFile("output.txt",JSON.stringify(personal_details, null, 2) ,noop)
+    // JSON.stringify syntax
+    //1)The second param is an optional replacer function which you don't need in this case so null works.
+    //2)The third param is the number of spaces to use for indentation.
 }
 parse()
 
-var trans = []
+// var trans = []
 
-var j =10
-while (textByLine[j]!=0) {
-    var heading = textByLine[j].split(',')
-    trans.push([heading[0].slice(6)+"-"+heading[0].slice(3,5)+"-"+heading[0].slice(0,2),heading[2].trim(),"(-)"+heading[3].trim(),"(+)"+heading[4].trim(),heading[5].trim()])
-    fs.appendFile("output.txt",String(heading[0]+", "+heading[2]+", (-)"+heading[3]+", (+)"+heading[4]+", "+heading[5])+"\n" , noop);    
-    j++
-}
-console.log(trans)
+// var j =10
+// while (textByLine[j]!=0) {
+//     var heading = textByLine[j].split(',')
+//     trans.push([heading[0].slice(6)+"-"+heading[0].slice(3,5)+"-"+heading[0].slice(0,2),heading[2].trim(),"(-)"+heading[3].trim(),"(+)"+heading[4].trim(),heading[5].trim()])
+//     fs.appendFile("output.txt",String(heading[0]+", "+heading[2]+", (-)"+heading[3]+", (+)"+heading[4]+", "+heading[5])+"\n" , noop);
+//     j++
+// }
+// console.log(trans)
 
